5c037be2fa60c71b20b091c7b05900bfa620e741
sasikumar <sasikumar@ibizcs.com>
2020-03-13 14:33:51 +0530

    first commit

---
+++ connecting SQL.ps1
@@ -0,0 +1,31 @@
+﻿$Global:SCCMSQLSERVER = "localhost\bcdemo" 
+$Global:DBNAME = "sqlTestDB" 
+Try 
+{ 
+$SQLConnection = New-Object System.Data.SQLClient.SQLConnection 
+$SQLConnection.ConnectionString ="server=$SCCMSQLSERVER;database=$DBNAME;Integrated Security=True;" 
+
+#"Server = $SQLServer; Database = $SQLDBName;User ID= YourUserID; Password= YourPassword"
+$SQLConnection.Open() 
+} 
+catch 
+{ 
+    [System.Windows.Forms.MessageBox]::Show("Failed to connect SQL Server:")  
+} 
+ 
+$SQLCommand = New-Object System.Data.SqlClient.SqlCommand 
+$SQLCommand.CommandText = "select * from Customer" 
+$SQLCommand.Connection = $SQLConnection 
+ 
+$SQLAdapter = New-Object System.Data.SqlClient.SqlDataAdapter 
+$SqlAdapter.SelectCommand = $SQLCommand                  
+$SQLDataset = New-Object System.Data.DataSet 
+$SqlAdapter.fill($SQLDataset) | out-null 
+ 
+$tablevalue = @() 
+foreach ($data in $SQLDataset.tables[0]) 
+{ 
+ $data
+
+}  
+$SQLConnection.close()
